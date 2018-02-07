26e685ec68125a76638cd689a38b0b23673b1673
xanalas <gign_91@mail.ru>
2018-02-07 12:38:23 +0300

    Работа над ошибками

--- js/stat.js
+++ js/stat.js
@@ -5,12 +5,10 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 var BAR_GAP = 50;
-var barWidth = 40;
+var BARWIDTH = 40;
 var TEXT_HEIGHT = CLOUD_HEIGHT - 20;
 var MAX_BAR = 150;
 var HEIGHT_BAR = 230;
-var names = ['Вы', 'Ваня', 'Катя', 'Олег'];
-var times = [2900, 2000, 1500, 2800];
 
 //Задаем функцию отрисовки облака
 var renderCloud = function(ctx, x, y, color) {
@@ -31,7 +29,7 @@ var getMaxElement = function(arr){
 };
 
 //Задаем функцию отриcовки статистики
-window.renderStatistics = function(ctx, names, time) {
+window.renderStatistics = function(ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
@@ -40,27 +38,18 @@ window.renderStatistics = function(ctx, names, time) {
   ctx.fillText("Ура вы победили!", 115, 40);
   ctx.fillText("Список результатов: ", 115, 60);
 
-
-
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], CLOUD_X + barWidth + (BAR_GAP + barWidth) * i, TEXT_HEIGHT );
-    ctx.fillRect(CLOUD_X + barWidth + (BAR_GAP + barWidth) * i , HEIGHT_BAR, barWidth, (-MAX_BAR * times[i]) / maxTime);
+  if(names[i] == 'Вы') {
+      ctx.fillStyle ='rgba(255, 0, 0, 1)';
+    } else {
 
-      if(names[i] = ['Вы']) {
-        ctx.fillStyle = ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-
-      } else {
-        ctx.fillStyle = 'rgb(50, 21, 179)';
-        ctx.globalAlpha = 0.9*Math.random();
-      }
+      ctx.fillStyle = 'rgb(0, 0, 255)' ;
+      ctx.globalAlpha = 0.9*Math.random();
     }
-  };
-
-
-
-
-
-
+  ctx.fillText(names[i], CLOUD_X + BARWIDTH + (BAR_GAP + BARWIDTH) * i, TEXT_HEIGHT );
+  ctx.fillRect(CLOUD_X + BARWIDTH + (BAR_GAP + BARWIDTH) * i , HEIGHT_BAR, BARWIDTH, (-MAX_BAR * times[i]) / maxTime);
+  }
+};
 
