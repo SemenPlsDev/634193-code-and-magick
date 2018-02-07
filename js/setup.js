b44d4f7eeec3e423f8d7b957be2bbb691b90991c
xanalas <gign_91@mail.ru>
2018-02-07 18:46:36 +0300

    Объявляем массивы переменных

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,15 @@
-'use strict'
+'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+//Объявляем массивы переменных
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColor = ['rgb(101, 137, 164)',
+                 'rgb(241, 43, 107)',
+                 'rgb(146, 100, 161)',
+                 'rgb(56, 159, 117)',
+                 'rgb(215, 210, 55)',
+                 'rgb(0, 0, 0)]'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
-document.querySelector('.setup-similar').classList.remove('hidden');
 
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
-for(var i = 0; i < 4; i++) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-
-  similarListElement.appendChild(wizardElement);
-}
